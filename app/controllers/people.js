d2a85b6c3142e70d5d5d6d87b7a46d57fdd5e5ee
Sarah M Pearson <sarahp@me.com>
2014-08-15 17:29:15 -0500

    working on routes

--- app/controllers/people.js
+++ app/controllers/people.js
@@ -1,6 +1,14 @@
 'use strict';
 
-exports.index = function(req, res){
-  res.render('home/index');
+var Person = require('../models/person');
+
+exports.init = function(req, res){
+  res.render('people/init');
+};
+
+exports.create = function(req, res){
+  Person.save(req.body, function(){
+    res.redirect('/people');
+  });
 };
 
